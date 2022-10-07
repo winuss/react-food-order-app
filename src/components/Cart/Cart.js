f75bb37d7eb23b86a2402c5a56492c67f7de5b24
winuss <nicyes@naver.com>
2022-10-07 21:56:42 +0900

    Cart / Model...

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import classes from './Cart.module.css'
+
+const Cart = () => {
+  const tempItems = [
+    {id: 'c1', name: 'Sushi', amount: 2, price: 12.99}
+  ]
+
+  const cartItems = <ul className={classes['cart-items']}>{tempItems.map((item)=> <li>{item.name}</li>)}</ul>
+
+  return (
+    <div>
+      {cartItems}
+      <div>
+        <span>Total Amount</span>
+        <span>35.43</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  )
+}
+
+export default Cart
