8282ac841df11e857ea95e00f06e7afa7b15675d
winuss <winuss@npixel.co.kr>
2022-10-07 18:34:21 +0900

    first

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import classes from './MealItem.module.css'
+
+const MealItem = (props) => {
+  const price = `${props.price.toFixed(2)}`;
+  
+  return (
+    <li>
+      <div>
+        <h3>{props.name}</h3>
+        <div>{props.description}</div>
+        <div>{props.price}</div>
+      </div>
+      <div></div>
+    </li>
+  )
+}
+
+export default MealItem
