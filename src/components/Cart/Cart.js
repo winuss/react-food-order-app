e23e9f931e5ccf5fb20a784916acaa2059dd2f25
winuss <nicyes@naver.com>
2022-10-11 22:11:09 +0900

    provider additem +1

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -18,7 +18,7 @@ const Cart = (props) => {
   }
 
   const cartItemAddHandler = (item) => {
-    cartCtx.addItem(item);
+    cartCtx.addItem({...item, amount: 1});
   }
 
   const cartItems = <ul className={classes['cart-items']}>
